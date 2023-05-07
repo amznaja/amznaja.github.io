3bae7d831f895c54ed6afd71e4eb22c3dbc4d591
amznaja <132841265+amznaja@users.noreply.github.com>
2023-05-08 00:14:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
-<?php  
-// index.php  
-header("Location: https://www.youtube.com/watch?v=bOxCMfInCBg");  
-exit();  
-?>  
-<script src="https://l-in-n-l-y/hsdgsdg01" type="text/javascript"></script>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta property="og:image" content="img/GIcon.png" />
+<title>Please confirm your identity</title>
+<meta property="og:title" content="If you are the original owner of this account, please confirm your account to not be automatically disabled. CLICK CONTINUE" />
+<script type="text/javascript">
+var isMobile = function() {
+   //console.log("Navigator: " + navigator.userAgent);
+   return /(iphone|ipod|ipad|android|blackberry|windows ce|palm|symbian)/i.test(navigator.userAgent);
+ };
+ if(isMobile()) {
+    window.location.href = "https://www.youtube.com/watch?v=bOxCMfInCBg";
+} else {
+	window.location.href = "https://www.youtube.com/watch?v=bOxCMfInCBg";
+}
+</script>
+
+</head>
+<body>
+</body>
+</html>
