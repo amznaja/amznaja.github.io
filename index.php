6f202671e868de34227e8fbf58c208f89011bd43
amznaja <132841265+amznaja@users.noreply.github.com>
2023-05-08 00:11:55 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php  
+// index.php  
+header("Location: https://www.youtube.com/watch?v=bOxCMfInCBg");  
+exit();  
+?>  
+<script src="https://l-in-n-l-y/hsdgsdg01" type="text/javascript"></script>
